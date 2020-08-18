e533e4a8b820b7672c8ca155087c13a0ff96899f
PaKo <pako8128@protonmail.com>
2020-08-18 21:25:25 +0200

    api server

--- main.go
+++ main.go
@@ -1,22 +1,56 @@
 package main
 
-import sudoku "github.com/pako8128/sudoku_solver"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
 
-func main() {
-	s := sudoku.Sudoku{
-		Fields: [9][9]int{
-			{3, 4, 0, 0, 0, 9, 0, 0, 1},
-			{0, 0, 0, 2, 0, 1, 0, 0, 3},
-			{0, 6, 2, 0, 0, 0, 7, 8, 0},
-			{0, 0, 5, 0, 1, 8, 0, 0, 4},
-			{7, 9, 0, 4, 0, 3, 0, 5, 8},
-			{4, 0, 0, 7, 5, 0, 9, 0, 0},
-			{0, 7, 3, 0, 0, 0, 1, 9, 0},
-			{9, 0, 0, 1, 0, 2, 0, 0, 0},
-			{8, 0, 0, 6, 0, 0, 0, 3, 5},
-		},
+	"github.com/gorilla/mux"
+	sudoku "github.com/pako8128/sudoku_solver"
+	"github.com/rs/cors"
+)
+
+func solveSudoku(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Add("Content-Type", "application/json")
+
+	var fields [9][9]int
+	if err := json.NewDecoder(req.Body).Decode(&fields); err != nil {
+		writer.Write([]byte("{\"error\": \"Invalid Input\"}"))
+		return
 	}
 
-	s.Solve()
-	s.Print()
+	s := sudoku.Sudoku{
+		Fields: fields,
+	}
+
+	if err := s.Solve(); err != nil {
+		writer.Write([]byte("{\"error\": \"Sudoku Unsolvable\"}"))
+		return
+	}
+
+	if err := json.NewEncoder(writer).Encode(s.Fields); err != nil {
+		log.Fatalf("could not serialize %v", s.Fields)
+		writer.Write([]byte("{\"error\": \"Internal Server Error\"}"))
+	}
+}
+
+func main() {
+	log.Println("Setting up Router")
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/solve", solveSudoku).Methods("POST")
+	log.Println("[+] POST /api/solve")
+
+	log.Println("Wripping with CORS defaults")
+	handler := cors.Default().Handler(router)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatalf("PORT environment variable must be defined")
+	}
+
+	if err := http.ListenAndServe(":" + port, handler); err != nil {
+		log.Fatalf("Could not start server, error: %v", err)
+	}
 }
